273102ee565d8d9b21b297d5f3ef8fe15b09bca4
Asanaliev Temirlan <tonyadam050607@gmail.com>
2025-03-26 23:27:42 +0600

    -

--- app/src/main/java/com/example/fakestrore/presentation/screens/CartScreen.kt
+++ app/src/main/java/com/example/fakestrore/presentation/screens/CartScreen.kt
@@ -1,11 +1,13 @@
 package com.example.fakestrore.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -17,13 +19,16 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -53,7 +58,9 @@ fun CartScreen(navController: NavController) {
         },
         content = {
             Box(
-                Modifier.fillMaxSize().padding(16.dp)
+                Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
             ) {
                 if(cartProduct.isEmpty()){
                     Text(
@@ -72,7 +79,10 @@ fun CartScreen(navController: NavController) {
                     )
                     LazyColumn() {
                         items(cartProduct.size) {
-
+                            CartCard(
+                                delete = { viewModel.deleteCart(cartProduct[it].id) },
+                                product = cartProduct[it]
+                            )
                         }
                     }
                 }
@@ -93,8 +103,11 @@ fun CartCard(
 ) {
     Card(
         modifier = Modifier
-           .padding(vertical = 8.dp, horizontal = 16.dp)
-           .fillMaxWidth(),
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
         Row(){
@@ -102,18 +115,46 @@ fun CartCard(
                 model = product.image,
                 contentDescription = product.description,
                 modifier = Modifier
-                    .fillMaxWidth(),
-                contentScale = ContentScale.Crop,
+                    .padding(16.dp)
+                    .width(174.dp),
+                contentScale = ContentScale.Fit,
                 placeholder = painterResource(R.drawable.ic_logo),
                 error = painterResource(R.drawable.ic_logo)
             )
-            Box(){
+            VerticalDivider()
+
+            Box(
+                modifier = Modifier.fillMaxWidth()
+            ) {
                 Text(
                     text = product.title,
-                    modifier = Modifier.padding(16.dp),
-                    fontSize = 16.sp
+                    modifier = Modifier
+                        .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+                        // Добавляем нижний отступ, чтобы текст не заходил под Row
+                        .padding(bottom = 60.dp)
+                        .align(Alignment.TopStart),
+                    fontSize = 16.sp,
+                    maxLines = 3, // Ограничиваем число строк (по необходимости)
+                    overflow = TextOverflow.Ellipsis
                 )
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.Bottom,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .align(Alignment.BottomCenter)
+                ) {
+                    Text(text = "${product.price}$", fontSize = 20.sp)
+                    IconButton(onClick = delete) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_shop_cart_outline_remove),
+                            contentDescription = "Delete"
+                        )
+                    }
+                }
             }
+
         }
     }
 }
